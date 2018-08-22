e08ec6dfb00d6cb489af52e1f22ed67767bdd1e9
Brendon <brendonbarringer@gmail.com>
2018-08-22 13:06:16 -0500

    updated config, added freetimeRoute(inprogress)

--- models/freetime.js
+++ models/freetime.js
@@ -1,11 +1,11 @@
 module.exports = (sequelize, DataTypes) => {
-const FreeTime = sequelize.define('FreeTime', {
-    startTime: DataType.STRING,
-    endTime: DataTypes.STRING
-});
+    const FreeTime = sequelize.define('FreeTime', {
+        startTime: DataTypes.STRING,
+        endTime: DataTypes.STRING
+    });
 
-FreeTime.associate = function(models){
-    FreeTime.belongsTo(Users, {foreignKey: 'User_id'})
-}
-return FreeTime
+    FreeTime.associate = function (models) {
+        FreeTime.belongsTo(models.User)
+    }
+    return FreeTime
 }
