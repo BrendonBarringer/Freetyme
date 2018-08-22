bde0c9e6235d1eef931cfe46dbc2bbde293ee921
Brendon <brendonbarringer@gmail.com>
2018-08-22 14:12:15 -0500

    freetimeRoutes.js

--- models/freetime.js
+++ models/freetime.js
@@ -1,11 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
     const FreeTime = sequelize.define('FreeTime', {
-        startTime: DataTypes.STRING,
-        endTime: DataTypes.STRING
+        startTime: DataTypes.DATE,
+        endTime: DataTypes.DATE
     });
 
     FreeTime.associate = function (models) {
-        FreeTime.belongsTo(models.User)
+        // FreeTime.belongsTo(models.User)
     }
     return FreeTime
 }
+
--- models/user.js
+++ models/user.js
@@ -21,3 +21,5 @@ module.exports = function(sequelize, DataTypes) {
   
     return User;
   };
+
+
