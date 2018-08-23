05005dc60e61b1f151fb22d773efc5049a067f2c
Brendon <brendonbarringer@gmail.com>
2018-08-23 17:28:13 -0500

    cleaned up Navbar.js

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,75 +3,6 @@ import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-// <<<<<<< gary
-// const Navbar = props => (
-//   <nav className="navbar navbar-expand-lg navbar-light bg-light">
-//     <Link className="navbar-brand" to="/">
-//       FREETYME!!!!!
-//     </Link>
-//     <div>
-//       <ul className="navbar-nav">
-//         <li
-//           className={
-//             window.location.pathname === "/" ||
-//             window.location.pathname === "/login"
-//               ? "nav-item active"
-//               : "nav-item"
-//           }
-//         >
-//           <Link to="/" className="nav-link">
-//             Login
-//           </Link>
-//         </li>
-//         <li
-//           className={            
-//             window.location.pathname === "/calendar"
-//               ? "nav-item active"
-//               : "nav-item"
-//           }
-//         >
-//           <Link to="/calendar" className="nav-link">
-//             Calendar
-//           </Link>
-//         </li>
-//         <li
-//           className={
-//             window.location.pathname === "/profile"
-//               ? "nav-item active"
-//               : "nav-item"
-//           }
-//         >
-//           <Link to="/profile" className="nav-link">
-//             Profile
-//           </Link>
-//         </li>
-//         <li
-//           className={
-//             window.location.pathname === "/about"
-//               ? "nav-item active"
-//               : "nav-item"
-//           }
-//         >
-//           <Link to="/about" className="nav-link">
-//             About
-//           </Link>
-//         </li>
-//         <li
-//           className={
-//             window.location.pathname === "/signup"
-//               ? "nav-item active"
-//               : "nav-item"
-//           }
-//         >
-//           <Link to="/signup" className="nav-link">
-            
-//           </Link>
-//         </li>
-//       </ul>
-//     </div>
-//   </nav>
-// );
-// =======
 class Navbar extends React.Component {
 
   render (props) {
@@ -146,8 +77,6 @@ class Navbar extends React.Component {
                 About
               </Link>
             </li>
-// <<<<<<< Brendon
-// =======
             <li
           className={
             window.location.pathname === "/signup"
@@ -159,7 +88,6 @@ class Navbar extends React.Component {
             
           </Link>
         </li>
-// >>>>>>> master
           </ul>
           <form className="form-inline"> 
             { this.props.loggedIn ? 
@@ -172,6 +100,5 @@ class Navbar extends React.Component {
     );
   }
 }
-// >>>>>>> master
 
 export default Navbar;
