357fed4b3df9610e3641acb470bfcf579d702de7
Brendon <brendonbarringer@gmail.com>
2018-08-23 20:05:52 -0500

    delete and update routes made

--- routes/api/freetimeRoutes.js
+++ routes/api/freetimeRoutes.js
@@ -1,31 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../models');
-const User = require('../../models/user');
-const FreeTime = require('../../models/freetime');
+
 
 
 
 
 router.get('/api/freetime', function (req, res, next) {
-  if (!session.user) {
-    res.redirect('/login');
-  }
-  else {
+  // if (!session.user) {
+  //   res.redirect('/login');
+  // }
+  // else {
     db.FreeTime.findAll({
       }).then(matches => {
       res.json(matches)
     }).catch(next)
-  }
+  // }
 })
 
 router.post('/api/freetime', function(req, res, next){
-  console.log(req.body);
+  // console.log(req.body);
 db.FreeTime.create({
   startTime: req.body.startTime,
   endTime: req.body.endTime
 }).then(newFreeTime => {
   res.json(newFreeTime)
+  })
+  .catch(next)
+})
+
+
+router.put('/api/freetime/:id', function(req, res, next) {
+  db.FreeTime.update(req.body, 
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(dbFreeTime) {
+    res.json(dbFreeTime)
+  })
+  .catch(next)
+})
+
+router.delete('/api/freetime/:id', function(req,res,next) {
+  db.FreeTime.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(function(dbFreeTime) {
+    res.json(dbFreeTime);
   })
   .catch(next)
 })
